121eb534b29f5ec7c78656bfc3cf5492d192e968
황혜경 <hwangmari@29cm.co.kr>
2018-11-15 16:57:12 +0900

    2018.11.15 app https://angular.io/guide/template-syntax

--- my-app/src/app/step/step.component.ts
+++ my-app/src/app/step/step.component.ts
@@ -6,10 +6,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./step.component.css']
 })
 export class StepComponent implements OnInit {
+    stepdsc : any=[];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.setStep();
+    }
+
+    setStep() {
+        this.stepdsc = [
+            {
+                "step_title":"111",
+                "step_dsc": "일입니다요",
+            },
+            {
+                "step_title":"222",
+                "step_dsc": "이입니다요",
+            },
+            {
+                "step_title":"333",
+                "step_dsc": "삼입니다요",
+            },
+            {
+                "step_title":"444",
+                "step_dsc": "사입니다요",
+            },
+            {
+                "step_title":"555",
+                "step_dsc": "오입니다요",
+            },
+        ];
+    }
 
 }
