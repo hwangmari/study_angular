2edb978a47ec06c781bfcce1b835e102328deb55
linda.hwang <linda.hwang@kakaoenterprise.com>
2020-08-24 15:55:07 +0900

    [angular] 세팅

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -3,15 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { StepComponent } from './step/step.component';
 import { Step2Component } from './step2/step2.component';
+import { TableComponent } from './table/table.component';
 
 const routes: Routes = [
     {
         path: 'step',
         component: StepComponent
-    }
+    },
     {
         path: 'step2',
         component: Step2Component
+    }
+    ,
+    {
+        path: 'table',
+        component: TableComponent
     }
 ];
 
--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { StepComponent } from './step/step.component';
 import { Step2Component } from './step2/step2.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StepComponent,
-    Step2Component
+    Step2Component,
+    TableComponent
   ],
   imports: [
     BrowserModule,
