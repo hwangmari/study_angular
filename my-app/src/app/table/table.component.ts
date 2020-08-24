70a8a5d855d2ef30adaf893956acc87ba5e6dd7a
hwangmari <hwangmari@naver.com>
2020-08-24 16:40:18 +0900

    [angular] table 추가

--- my-app/src/app/table/table.component.ts
+++ my-app/src/app/table/table.component.ts
@@ -7,13 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  stepdsc : any=[];
 
-  ngOnInit() {
-  }
+    constructor() { }
+
+    ngOnInit() {
+        this.setStep();
+    }
+
+    setStep() {
+        this.stepdsc = [
+            {
+                "step_title":"111",
+                "step_dsc": "일입니다요",
+            },
+            {
+                "step_title":"222",
+                "step_dsc": "이입니다요",
+            },
+            {
+                "step_title":"333",
+                "step_dsc": "삼입니다요",
+            },
+            {
+                "step_title":"444",
+                "step_dsc": "사입니다요",
+            },
+            {
+                "step_title":"555",
+                "step_dsc": "오입니다요",
+            },
+        ];
+    }
 
 }
-export interface PeriodicElement {
- 
-}
-
